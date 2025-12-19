2bb38995ad69a1d3c6ffff7e28d2052409cc9b2e
poderosissimo ninja <rodrigo.rodrigues@compasso.com.br>
2025-12-19 11:33:41 -0300

    feature: adding menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 import "./styles/theme.css";
 import "./styles/global.css";
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Container><Logo /></Container>
+      <Container><Menu /></Container>
     </>
   );
 }
