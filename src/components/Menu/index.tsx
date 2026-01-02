d3ce0277bfdcceb718a8ba7401015ebc58ee74af
poderosissimo ninja <rodrigo.rodrigues@compasso.com.br>
2026-01-02 13:23:52 -0300

    feat: adding a lookup to change icon theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { HistoryIcon, HouseIcon, MoonIcon, SettingsIcon, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
 import { useState, useEffect } from "react";
 
@@ -7,7 +7,15 @@ type availableTheme = 'dark' | 'light';
 // { children } destructuring from props
 export function Menu() {
 
-  const [theme, setTheme] = useState<availableTheme>('dark');
+  const [theme, setTheme] = useState<availableTheme>(() => {
+    const storageTheme = localStorage.getItem('theme') as availableTheme || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    'dark': <SunIcon />,
+    'light': <MoonIcon />
+  }
 
   function handleThemeChange(e: React.MouseEvent) {
     e.preventDefault();
@@ -27,6 +35,7 @@ export function Menu() {
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
   }, [theme]) // Renderiza sempre que theme mudar
 
   return (
@@ -62,7 +71,7 @@ export function Menu() {
         title="Mudar tema"
         onClick={handleThemeChange}
       >
-        <SunIcon />
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
